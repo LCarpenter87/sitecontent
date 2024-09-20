80c17733ae6eaa908cdbcc24cb217c1ade428656
Lisa <L.carpenter@outlook.com>
2024-09-20 21:24:16 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+
+st.header("Welcome to Lisa's Links")
+
+st.link_button("Lisa's Linkedin", "https://linkedin.com/in/lisacarpenter")
+
+st.link_button("Lisa's X / Twitter", "https://x.com/Lisa_DataCoach")
+
+st.link_button("Lisa's Medium Articles", "https://medium.com/@lisa_")
+
+st.link_button("Lisa's Google Dev Profile", "https://g.dev/Lisa")
+
+st.write(" 😊 ")
